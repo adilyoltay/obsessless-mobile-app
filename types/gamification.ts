cc894a0b01f35109e09f290197beeffa861c2bb4
Adil Yoltay <adil.yoltay@gmail.com>
2025-08-22 08:08:12 +0300

    Remove remaining ERP triggers from gamification types

--- types/gamification.ts
+++ types/gamification.ts
@@ -47,17 +47,17 @@ export interface HealingPoints {
 // Micro-reward triggers
 export type MicroRewardTrigger =
   | 'compulsion_recorded'
-  | 'erp_completed'
+
   | 'high_resistance'
   | 'anxiety_reduced'
   | 'daily_goal_met'
-  | 'erp_quick_start'
+
   | 'planning_ahead'
   | 'compulsion_quick_entry'
   | 'pattern_recognition'
   | 'consistent_tracking'
   | 'resistance_improvement'
-  | 'erp_wizard_start'
+
   | 'urge_resistance'
   | 'voice_mood_checkin';
 
